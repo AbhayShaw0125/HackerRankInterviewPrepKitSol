6e8f511c49bb60bfd59500a366a0950c75616b35
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-11-17 15:38:56 +0530

    Create timeConv.scala

---
+++ week1/timeConv.scala
@@ -0,0 +1,56 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
+object Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    def timeConversion(s: String): String = {
+    // Write your code here
+    val period = s.substring(8, 10)   
+        var hour = s.substring(0, 2).toInt 
+        val rest = s.substring(2, 8)      
+        if (period == "PM" && hour != 12)
+            hour += 12
+        else if (period == "AM" && hour == 12)
+            hour = 0
+
+        f"$hour%02d$rest"
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val s = StdIn.readLine
+
+        val result = Result.timeConversion(s)
+
+        printWriter.println(result)
+
+        printWriter.close()
+    }
+}
