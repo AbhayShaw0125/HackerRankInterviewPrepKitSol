4003929b67fd735214dc5f5f73b69c09a42fda86
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-11-17 15:37:45 +0530

    Create min_max.py

---
+++ week1/min_max.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    total=sum(arr)
+    mini=total-max(arr)
+    maxi=total-min(arr)
+    
+    print(mini,maxi)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
