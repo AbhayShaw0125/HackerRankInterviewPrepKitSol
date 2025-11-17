ec38340f467009f4f83d476833a27003a12d5b75
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-11-17 15:42:46 +0530

    Create counting.scala

---
+++ week1/counting.scala
@@ -0,0 +1,57 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
+object Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    def countingSort(arr: Array[Int]): Array[Int] = {
+    // Write your code here
+        var fillSize=Array.fill(100)(0)
+        
+        for ( i <- arr){
+            fillSize(i)+=1
+        }
+        
+        fillSize
+
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val n = StdIn.readLine.trim.toInt
+
+        val arr = StdIn.readLine.replaceAll("\\s+$", "").split(" ").map(_.trim.toInt)
+
+        val result = Result.countingSort(arr)
+
+        printWriter.println(result.mkString(" "))
+
+        printWriter.close()
+    }
+}
