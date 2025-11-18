b8f02dcd67583a022b3c5f4223e6bc6fe942d78d
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-11-18 16:24:04 +0530

    Create grading.scala

---
+++ 3month/grading.scala
@@ -0,0 +1,62 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+import scala.collection.immutable._
+import scala.collection.mutable._
+import scala.collection.concurrent._
+import scala.concurrent._
+import scala.io._
+import scala.math._
+import scala.sys._
+import scala.util.matching._
+import scala.reflect._
+
+object Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    def gradingStudents(grades: Array[Int]): Array[Int] = {
+    // Write your code here
+    grades.map{
+        g=>if(g<38)g
+        else{
+            val nextMultiple = ((g / 5) + 1) * 5
+            if (nextMultiple - g < 3) nextMultiple else g
+        }
+    }
+
+    }
+
+}
+
+object Solution {
+    def main(args: Array[String]) {
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val gradesCount = StdIn.readLine.trim.toInt
+
+        val grades = Array.ofDim[Int](gradesCount)
+
+        for (i <- 0 until gradesCount) {
+            val gradesItem = StdIn.readLine.trim.toInt
+            grades(i) = gradesItem
+        }
+
+        val result = Result.gradingStudents(grades)
+
+        printWriter.println(result.mkString("\n"))
+
+        printWriter.close()
+    }
+}
